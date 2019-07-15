395c287a87b2dc03b141e9e5c6f57b14811250ec
asekdev <andy.sek94@gmail.com>
2019-07-15 10:33:00 +1000

    added some todos to come back to

---
+++ src/redux/reducers/quizReducer.js
@@ -0,0 +1,1 @@
+//quiz reducer to be implemented
---
+++ src/redux/store/index.js
@@ -0,0 +1,7 @@
+//this file is for the store of the application
+
+//TODO
+/* 
+    - add middleware (thunk or saga - tbd)
+    - combine reducers 
+*/
